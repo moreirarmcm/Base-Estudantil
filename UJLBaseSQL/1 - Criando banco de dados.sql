f20bf29529073fe1333d5ee5d457633e2955bcdf
Renan Moreira <105755795+moreirarmcm@users.noreply.github.com>
2023-01-08 01:41:05 -0300

    Update 1 - Criando banco de dados.sql

--- UJLBaseSQL/1 - Criando banco de dados.sql
+++ UJLBaseSQL/1 - Criando banco de dados.sql
@@ -11,13 +11,6 @@
 create database UJLBase
 GO
 use UJLBase
-
-/*
-DROP database UJLBase2
-create database UJLBase2
-GO
-use UJLBase2
-*/
 --================================================================================================================
 --=======================================| Criação das tabelas |==================================================
 --================================================================================================================
@@ -32,8 +25,8 @@ CREATE TABLE Membro (
 	Data_Ingresso DATE,
 	CodigoInstituicao INT,
 	PosicaoPolitica VARCHAR (255) NOT NULL,
-	CodigoBancada INT
-)
+	AreaEstudo VARCHAR(255)
+	)
 GO
 
 CREATE TABLE Contato(
@@ -50,11 +43,11 @@ GO
 
 CREATE TABLE Endereco (
 	Codigo INT PRIMARY KEY IDENTITY,
-	CodigoMembro INT NOT NULL UNIQUE,
-	Bairro VARCHAR(30) NOT NULL,
-	Cidade VARCHAR(30) NOT NULL,
+	CodigoMembro INT NOT NULL,
+	Bairro VARCHAR(255) NOT NULL,
+	Cidade VARCHAR(50) NOT NULL,
 	UF CHAR(2) NOT NULL,
-	RegiaoCidade VARCHAR(6) NOT NULL CHECK (RegiaoCidade IN('Norte','Sul','Leste','Oeste','Centro','Nenhuma das opções anteriores'))
+	RegiaoCidade VARCHAR(255) NOT NULL CHECK (RegiaoCidade IN('Norte','Sul','Leste','Oeste','Centro','Nenhuma das opções anteriores'))
 )
 GO
 
@@ -62,62 +55,15 @@ CREATE TABLE Instituicao(
 	Codigo INT PRIMARY KEY IDENTITY,
 	Nome VARCHAR(100) NOT NULL,
 	GrauAcademico VARCHAR(50) NOT NULL CHECK (GrauAcademico IN('Ensino Fundamental','Ensino Médio','Ensino Superior','Nenhuma das alternativas anteriores','Sou educador')),
-	Polo VARCHAR(50),
-	Curso VARCHAR(50),
+	Polo VARCHAR(50)
 )
 GO
 
-
-CREATE TABLE Resposta(
+CREATE TABLE Bancada(
 	Codigo INT PRIMARY KEY IDENTITY,
-	CodigoMembro INT NOT NULL,
-	R1 VARCHAR(1000),
-	R2 VARCHAR(1000),
-	R3 VARCHAR(1000),
-	R4 VARCHAR(1000),
-	R5 VARCHAR(1000),
-	R6 VARCHAR(1000),
-	R7 VARCHAR(1000),
-	R8 VARCHAR(1000),
-	R9 VARCHAR(1000)
+	Nome VARCHAR (255)
 )
 GO
-
-CREATE TABLE Diretoria(
-	Codigo INT PRIMARY KEY IDENTITY,
-	Nome VARCHAR(30),
-	CodigoCoordenador INT,
-	Coordenador VARCHAR(50),
-	Criacao DATE
-)
-GO
-
-CREATE TABLE MembroDiretoria(
-	CodigoDiretoria INT,
-	CodigoMembro INT,
-	Data_Inicio DATE,
-	Data_Final DATE
-)
-GO
-
-CREATE TABLE Ilha(
-	Codigo INT PRIMARY KEY IDENTITY,
-	CodigoInstituicao INT,
-	CodigoMentor INT,
-	CodigoMonitor INT,
-	CodigoMembro INT,
-	Criacao DATE,
-	Ativo INT
-)
-
-CREATE TABLE Naufrago(
-	CodigoMembro INT,
-	Nome VARCHAR(50),
-	CodigoContato INT,
-	CodigoInstituicao INT,
-	CodigoMonitor INT,
-	CodigoIlha INT
-)
 
 CREATE TABLE MembroTemporaria(
 	Nome VARCHAR(50),
@@ -127,32 +73,22 @@ CREATE TABLE MembroTemporaria(
 )
 go
 
-CREATE TABLE Bancada(
-	Codigo INT PRIMARY KEY IDENTITY,
-	Nome VARCHAR (1000),
-	Descricao VARCHAR (255),
-	CodigoLider INT
+CREATE TABLE EscolhaBancada(
+	CodigoMembro INT,
+	Escolha VARCHAR(1000)
 )
 GO
 
-CREATE TABLE TemporariaDois(
-	EMAIL VARCHAR(1000),
-	ComoSeDefine VARCHAR(1000),
-	PorqueEntrarUJL VARCHAR(1000),
-	qUAISoBJETIVOS VARCHAR(1000),
-	DefineBolsonaro VARCHAR(1000),
-	Personalidades VARCHAR(1000),
-	RejeitaMovimentoLiberal VARCHAR(1000),
-	ConsideraLula VARCHAR(1000),
-	AvaliaExGovernos VARCHAR(1000),
-	FazParteMovimentos VARCHAR(1000),
-	TrabalhaGabinete VARCHAR(1000)
+CREATE TABLE MembroBancada(
+	CodigoBancada INT,
+	CodigoMembro INT,
+	InicioSituacao DATE
 )
-GO 
 
 --================================================================================================================
 --====================================| Criando Chaves Estrangeiras |=============================================
 --================================================================================================================
+
 ALTER TABLE Membro
 ADD CONSTRAINT FK_INSTITUICAO_MEMBRO
 FOREIGN KEY (CodigoInstituicao) REFERENCES Instituicao (Codigo)
@@ -167,108 +103,6 @@ ALTER TABLE Endereco
 ADD CONSTRAINT FK_MEMBRO_ENDERECO
 FOREIGN KEY (CodigoMembro) REFERENCES MEMBRO (Codigo)
 
---ALTER TABLE ENDERECO
---DROP CONSTRAINT FK_MEMBRO_ENDERECO
-
-ALTER TABLE Resposta
-ADD CONSTRAINT FK_MEMBRO_RESPOSTA
-FOREIGN KEY (CodigoMembro) REFERENCES MEMBRO (Codigo)
-GO
-
---ALTER TABLE Resposta
---DROP CONSTRAINT FK_MEMBRO_RESPOSTA
-
---ALTER TABLE Ilha
---DROP CONSTRAINT FK_MEMBRO_RESPOSTA
 
 
-ALTER TABLE Diretoria
-	ADD CONSTRAINT FK_MEMBRO_DIRETORIA
-FOREIGN KEY (CodigoCoordenador) REFERENCES MEMBRO (Codigo)
-GO
-
-ALTER TABLE Diretoria
-DROP CONSTRAINT FK_MEMBRO_DIRETORIA
-
-
-ALTER TABLE MembroDiretoria
-	ADD CONSTRAINT FK_Diretoria_MembroDiretoria
-FOREIGN KEY (CodigoDiretoria) REFERENCES Diretoria (Codigo)
-GO
-
-ALTER TABLE MembroDiretoria
-DROP CONSTRAINT FK_Diretoria_MembroDiretoria
-
-
-ALTER TABLE MembroDiretoria
-ADD CONSTRAINT FK_Membro_MembroDiretoria
-FOREIGN KEY (CodigoMembro) REFERENCES MEMBRO (Codigo)
-GO
-
-ALTER TABLE MembroDiretoria
-DROP CONSTRAINT FK_Membro_MembroDiretoria
-
-
-ALTER TABLE Naufrago
-ADD CONSTRAINT FK_Membro_Naufrago
-FOREIGN KEY (CodigoMembro) REFERENCES Membro (Codigo)
-
-ALTER TABLE Naufrago
-DROP CONSTRAINT FK_Membro_Naufrago
-
-
-ALTER TABLE Naufrago
-ADD CONSTRAINT FK_Contato_Naufrago
-FOREIGN KEY (CodigoContato) REFERENCES Contato (Codigo)
-
-ALTER TABLE Naufrago
-DROP CONSTRAINT FK_Contato_Naufrago
-
-
-ALTER TABLE Naufrago
-ADD CONSTRAINT FK_Instituicao_Naufrago
-FOREIGN KEY (CodigoInstituicao) REFERENCES Instituicao (Codigo)
-
-ALTER TABLE Naufrago
-ADD CONSTRAINT FK_MembroMonitor_Naufrago
-FOREIGN KEY (CodigoMonitor) REFERENCES Membro (Codigo)
-
-ALTER TABLE Naufrago
-DROP CONSTRAINT FK_MembroMonitor_Naufrago
-
-ALTER TABLE Ilha
-ADD CONSTRAINT FK_Instituicao_Ilha
-FOREIGN KEY (CodigoInstituicao) REFERENCES Instituicao (Codigo)
-
-ALTER TABLE Ilha
-ADD CONSTRAINT FK_MembroMentor_Ilha
-FOREIGN KEY (CodigoMentor) REFERENCES Membro (Codigo)
-
-ALTER TABLE Ilha
-DROP CONSTRAINT FK_MembroMentor_Ilha
-
-ALTER TABLE Ilha
-ADD CONSTRAINT FK_MembroMonitor_Ilha
-FOREIGN KEY (CodigoMonitor) REFERENCES Membro (Codigo)
-
-ALTER TABLE Ilha
-DROP CONSTRAINT FK_MembroMonitor_Ilha
-
-ALTER TABLE Ilha
-ADD CONSTRAINT FK_MembroMembro_Ilha
-FOREIGN KEY (CodigoMembro) REFERENCES Membro (Codigo)
-
-ALTER TABLE Ilha
-DROP CONSTRAINT FK_MembroMembro_Ilha
-
-ALTER TABLE Bancada
-ADD CONSTRAINT FK_Membro_Bancada
-FOREIGN KEY (CodigoLider) REFERENCES Membro (Codigo)
-
-ALTER TABLE Bancada
-DROP CONSTRAINT FK_Membro_Bancada
-
-ALTER TABLE Membro
-ADD CONSTRAINT FK_Bancada_MEMBRO
-FOREIGN KEY (CodigoBancada) REFERENCES Bancada (Codigo)
 
