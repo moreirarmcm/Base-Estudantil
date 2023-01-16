cca3ee3b4c63a478147402585b45adc7ffd7c444
Renan Moreira <105755795+moreirarmcm@users.noreply.github.com>
2023-01-15 21:27:35 -0300

    Update 0 - Relatório.sql

--- UJLBaseSQL/0 - Relatório.sql
+++ UJLBaseSQL/0 - Relatório.sql
@@ -60,3 +60,37 @@ select en.Cidade, COUNT(mem.Nome)
 	join Endereco en on mem.Codigo = en.CodigoMembro
 	GROUP BY en.Cidade
 
+--=========================================================================
+----=====| Tabela 5: Membros por bancada |==================
+
+select mb.CodigoBancada, m.Nome from MembroBancada mb join Membro m on m.codigo = mb.codigomembro where CodigoBancada = 1
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 2
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 3
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 4
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 5
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 6
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 7
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 8
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 9
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 10
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 11
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 12
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 13
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 14
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 15
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 16
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 17
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 18
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 19
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 20
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 21
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 22
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 23
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 24
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 25
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 26
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 27
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 28
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 29
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 30
+select mb.CodigoBancada, m.Nome from MembroBancada mb join membro m on m.codigo = mb.codigomembro where CodigoBancada = 31
